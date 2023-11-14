339dbae58234829cee6b33886ceeea46eb0116c1
George Leyva <leyvageorge@hotmail.com>
2023-11-13 17:11:04 -0800

    pt 5 done

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,24 @@ const Content = () => {
         const int = Math.floor(Math.random() * 5)
         return names[int];
       }
+
+    const handleClick = () => {
+        console.log('You cliked it');
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    }
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click It</button>
+        <button onClick={() => handleClick2('Jim')}>Click It</button>
+        <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   )
 }
